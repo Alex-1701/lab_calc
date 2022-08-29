8af7facfd808a7581206bd97de7a98716e842dd3
aliaksei.kaliada <aliaksei.kaliada@leverx.com>
2022-08-29 23:43:53 +0300

    Add minification on save

--- server.ts
+++ server.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import {minify} from "html-minifier";
+import fs from "fs";
 
 const app = express();
 const port = 8080;
@@ -8,12 +10,22 @@ const port = 8080;
 app.use(cors());
 app.use(bodyParser.json());
 
-// для передачи статических файлов
-// app.use(express.static("./"));
-
 app.get("/", (req, res) => {
-  console.log('lel');
-  res.sendFile(__dirname + "/index.html");
+  console.log('reload page');
+  fs.readFile(__dirname + "/index.html", "utf-8", (err, data) => {
+    console.log(data.length);
+    const minFile = minify(data, {
+      removeAttributeQuotes: true,
+      collapseWhitespace: true,
+      removeComments: true,
+      minifyCSS: true,
+      minifyJS: true,
+      removeTagWhitespace: true,
+    });
+    fs.writeFile(__dirname + "/index.min.html", minFile, () => {
+      res.sendFile(__dirname + "/index.min.html");
+    })
+  })
 });
 
 app.listen(port, () => {

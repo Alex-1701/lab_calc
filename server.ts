1c2198f47c2eb08c6e68a1a4a58a6ccb3fb16fc6
aliaksei.kaliada <aliaksei.kaliada@leverx.com>
2022-09-03 23:36:33 +0300

    show local ip address when run server

--- server.ts
+++ server.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import {minify} from "html-minifier";
 import fs from "fs";
+import {networkInterfaces} from "os";
 
 const app = express();
 const port = 8080;
@@ -31,4 +32,23 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log("localhost server start on port:", port);
+
+  // This code for identify and show IP in local network
+  const nets = networkInterfaces();
+  const results = Object.create(null);
+
+  for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+      // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+      // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
+      const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+      if (net.family === familyV4Value && !net.internal) {
+        if (!results[name]) {
+          results[name] = [];
+        }
+        results[name].push(net.address);
+      }
+    }
+  }
+  console.log(results);
 });

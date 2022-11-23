fb2d883a22d807a145b5a3836217c1069098659f
Aliaksei Kaliada <aliaksei.kaliada@leverx.com>
2022-11-23 18:18:58 +0300

    remove flex from body

--- server.ts
+++ server.ts
@@ -1,34 +1,73 @@
-import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
-import {minify} from "html-minifier";
-import fs from "fs";
+// import express from "express";
+// import cors from "cors";
+// import bodyParser from "body-parser";
+// import {minify} from "html-minifier";
+// import fs from "fs";
+// import {networkInterfaces} from "os";
+//
+// const app = express();
+// const port = 8080;
+//
+// app.use(cors());
+// app.use(bodyParser.json());
+//
+// app.get("/", (req, res) => {
+//   console.log('reload page');
+//   fs.readFile(__dirname + "/index.html", "utf-8", (err, data) => {
+//     console.log("original length", data.length);
+//     const minFile = minify(data, {
+//       removeAttributeQuotes: true,
+//       collapseWhitespace: true,
+//       removeComments: true,
+//       minifyCSS: true,
+//       minifyJS: true,
+//       removeTagWhitespace: true,
+//     });
+//     console.log("minify length", minFile.length);
+//     fs.writeFile(__dirname + "/index.min.html", minFile, () => {
+//       res.sendFile(__dirname + "/index.min.html");
+//     })
+//   })
+// });
+//
+// app.get("/manifest.json", (req, res) => {
+//   res.sendFile(__dirname + "/manifest.json");
+// });
+//
+// app.listen(port, () => {
+//   console.log("localhost server start on port:", port);
+//
+//   // This code for identify and show IP in local network
+//   const nets = networkInterfaces();
+//   const results = Object.create(null);
+//
+//   for (const name of Object.keys(nets)) {
+//     for (const net of nets[name]) {
+//       // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+//       // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
+//       const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+//       if (net.family === familyV4Value && !net.internal) {
+//         if (!results[name]) {
+//           results[name] = [];
+//         }
+//         results[name].push(net.address);
+//       }
+//     }
+//   }
+//   console.log(results);
+// });
+
+
+const express = require('express')
 import {networkInterfaces} from "os";
 
-const app = express();
-const port = 8080;
+const app = express()
+const port = 3000
+app.use(express.static('./'))
 
-app.use(cors());
-app.use(bodyParser.json());
-
-app.get("/", (req, res) => {
-  console.log('reload page');
-  fs.readFile(__dirname + "/index.html", "utf-8", (err, data) => {
-    console.log("original length", data.length);
-    const minFile = minify(data, {
-      removeAttributeQuotes: true,
-      collapseWhitespace: true,
-      removeComments: true,
-      minifyCSS: true,
-      minifyJS: true,
-      removeTagWhitespace: true,
-    });
-    console.log("minify length", minFile.length);
-    fs.writeFile(__dirname + "/index.min.html", minFile, () => {
-      res.sendFile(__dirname + "/index.min.html");
-    })
-  })
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(port, () => {
   console.log("localhost server start on port:", port);
---
+++ service-worker.js
@@ -0,0 +1,36 @@
+// importScripts(
+//   'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
+// );
+//
+// workbox.routing.registerRoute(
+//   ({request}) => request.destination === 'image',
+//   new workbox.strategies.CacheFirst()
+// )
+
+// Choose a cache name
+const cacheName = 'cache-v1';
+// List the files to precache
+const precacheResources = ['/', '/index.html'];
+
+// When the service worker is installing, open the cache and add the precache resources to it
+self.addEventListener('install', (event) => {
+  console.log('Service worker install event!');
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service worker activate event!');
+});
+
+// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
+self.addEventListener('fetch', (event) => {
+  console.log('Fetch intercepted for:', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    }),
+  );
+});

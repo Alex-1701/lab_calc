c90c5ebba8e29b215b1b43c11f8792063d71a366
Aliaksei Kaliada <alex17koleda@gmail.com>
2022-11-24 19:15:28 +0300

    Added new two calculators. Start to make them switchable

--- public/index.js
+++ public/index.js
@@ -1,3 +1,36 @@
+const redraw = () => {
+
+}
+
+const visibilityState = {
+  menu: true,
+  Nichiporenko: false,
+  SMG: false,
+  LPNP: false,
+  Fibrinogen: false,
+
+  setNichiporenko: () => {
+    this.Nichiporenko = true;
+  },
+
+  setSMG: () => {
+    this.SMG = true;
+  },
+
+  setLPNP: () => {
+    this.LPNP = true;
+  },
+
+  setFibrinogen: () => {
+    this.Fibrinogen = true;
+  },
+
+  setMenu: () => {
+    this.menu = true;
+  },
+}
+
+// Ничипоренко
 const calculate_N = () => {
   const x = document.getElementById("input_x").value;
 
@@ -11,9 +44,10 @@ const calculate_N = () => {
   document.getElementById("a2_result").value = (x * 2 / 3).toFixed(3);
   document.getElementById("a1_result").value = (x * 1 / 3).toFixed(3);
 }
-
 document.getElementById("input_x").addEventListener("input", calculate_N)
+// ************
 
+// СМЖ
 const calculate_KA = () => {
   const a = document.getElementById("input_a").value;
   const b = document.getElementById("input_b").value;
@@ -24,8 +58,39 @@ const calculate_KA = () => {
     document.getElementById("KA_result").value = "";
   }
 }
-
 document.getElementById("input_a").addEventListener("input", calculate_KA)
 document.getElementById("input_b").addEventListener("input", calculate_KA)
+// ************
 
-// calculate_KA()
+// ЛПНП
+const calculate_LPNP = () => {
+  const OXC = document.getElementById("input_OXC").value;
+  const LPVP = document.getElementById("input_LPVP").value;
+  const TG = document.getElementById("input_TG").value;
+
+  if (OXC && LPVP && TG) {
+    document.getElementById("LPNP_result").value = (OXC - (LPVP + TG / 2.2)).toFixed(1);
+  } else {
+    document.getElementById("LPNP_result").value = "";
+  }
+}
+document.getElementById("input_OXC").addEventListener("input", calculate_LPNP)
+document.getElementById("input_LPVP").addEventListener("input", calculate_LPNP)
+document.getElementById("input_TG").addEventListener("input", calculate_LPNP)
+// ************
+
+// Фибриноген
+const calculate_F = () => {
+  const A2 = document.getElementById("input_A2").value;
+  const X2 = document.getElementById("input_X2").value;
+
+  if (A2 && X2) {
+    document.getElementById("F_result").value = (A2 * 2.85 / X2).toFixed(1);
+  } else {
+    document.getElementById("F_result").value = "";
+  }
+}
+document.getElementById("input_A2").addEventListener("input", calculate_F)
+document.getElementById("input_X2").addEventListener("input", calculate_F)
+// ************
+
